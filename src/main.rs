77cfe29f09e116d11991a4e4292b22ecab47f06d
Travis Brown <travisrobertbrown@gmail.com>
2024-10-27 14:07:21 +0100

    Add commands

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use cli_helpers::prelude::*;
+use model::{NoteEntry, NoteStatusHistoryEntry};
 use std::path::PathBuf;
 
 mod model;
@@ -21,6 +22,56 @@ fn main() -> Result<(), Error> {
 
             model::ParticipantNoteIdMapping::write(unknown_aliases, unknown_aliases_values)?;
         }
+        Command::UpdateNoteStatusHistory {
+            aliases,
+            unknown_aliases,
+            notes,
+            note_status_history,
+        } => {
+            let aliases = model::ParticipantAliasMapping::read(aliases)?;
+            let mut unknown_aliases_values =
+                model::ParticipantNoteIdMapping::read(&unknown_aliases)?;
+            let note_status_history = model::NoteStatusHistoryEntry::read(note_status_history)?;
+
+            ::log::info!("{}", note_status_history.len());
+
+            let mut note_entries = model::NoteEntry::read(&notes)?;
+
+            for NoteStatusHistoryEntry {
+                note_id,
+                participant_id,
+                created_at_ms,
+                current_status,
+            } in note_status_history.values()
+            {
+                let entry = note_entries.entry(*note_id).or_insert(NoteEntry {
+                    note_id: *note_id,
+                    created_at_ms: 0,
+                    alias: None,
+                    tweet_id: None,
+                    user_id: None,
+                    misleading: None,
+                    helpful: None,
+                });
+
+                entry.created_at_ms = *created_at_ms;
+                entry.alias = aliases.get(participant_id).cloned();
+                entry.helpful = current_status.is_helpful();
+
+                if entry.alias.is_none() {
+                    let entries = unknown_aliases_values
+                        .entry(participant_id.clone())
+                        .or_default();
+
+                    entries.push(*note_id);
+                    entries.sort();
+                    entries.dedup();
+                }
+            }
+
+            model::NoteEntry::write(notes, note_entries)?;
+            model::ParticipantNoteIdMapping::write(unknown_aliases, unknown_aliases_values)?;
+        }
     }
 
     Ok(())
@@ -36,6 +87,12 @@ pub enum Error {
     LogInitialization(#[from] cli_helpers::Error),
     #[error("Duplicate alias mapping")]
     DuplicateAliasMapping(String, String),
+    #[error("Duplicate note status history entry")]
+    DuplicateNoteStatusHistoryEntry(u64),
+    #[error("Duplicate note entry")]
+    DuplicateNoteEntry(u64),
+    #[error("Invalid millisecond timestamp")]
+    InvalidTimestamp(u64),
 }
 
 #[derive(Debug, Parser)]
@@ -54,5 +111,15 @@ enum Command {
         aliases: PathBuf,
         #[clap(long, default_value = "workspace/unknown-aliases.csv")]
         unknown_aliases: PathBuf,
+    },
+    UpdateNoteStatusHistory {
+        #[clap(long, default_value = "data/aliases.csv")]
+        aliases: PathBuf,
+        #[clap(long, default_value = "workspace/unknown-aliases.csv")]
+        unknown_aliases: PathBuf,
+        #[clap(long, default_value = "data/notes/")]
+        notes: PathBuf,
+        #[clap(long)]
+        note_status_history: PathBuf,
     },
 }
--- src/model.rs
+++ src/model.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 use std::fs::File;
@@ -49,8 +50,8 @@ impl ParticipantNoteIdMapping {
             .from_reader(File::open(path)?);
         let mut mappings = HashMap::new();
 
-        for mapping in reader.deserialize::<ParticipantNoteIdMapping>() {
-            let ParticipantNoteIdMapping {
+        for mapping in reader.deserialize::<Self>() {
+            let Self {
                 participant_id,
                 note_id,
             } = mapping?;
@@ -71,12 +72,10 @@ impl ParticipantNoteIdMapping {
         let mut values = values
             .into_iter()
             .flat_map(|(participant_id, note_ids)| {
-                note_ids
-                    .into_iter()
-                    .map(move |note_id| ParticipantNoteIdMapping {
-                        participant_id: participant_id.clone(),
-                        note_id,
-                    })
+                note_ids.into_iter().map(move |note_id| Self {
+                    participant_id: participant_id.clone(),
+                    note_id,
+                })
             })
             .collect::<Vec<_>>();
 
@@ -91,5 +90,154 @@ impl ParticipantNoteIdMapping {
         }
 
         Ok(writer.flush()?)
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, serde::Deserialize, serde::Serialize)]
+pub enum Status {
+    #[serde(rename = "NEEDS_MORE_RATINGS")]
+    NeedsMoreRatings,
+    #[serde(rename = "CURRENTLY_RATED_NOT_HELPFUL")]
+    NotHelpful,
+    #[serde(rename = "CURRENTLY_RATED_HELPFUL")]
+    Helpful,
+}
+
+impl Status {
+    pub fn is_helpful(&self) -> Option<bool> {
+        match self {
+            Self::NeedsMoreRatings => None,
+            Self::NotHelpful => Some(false),
+            Self::Helpful => Some(true),
+        }
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, serde::Deserialize)]
+pub struct NoteStatusHistoryEntry {
+    #[serde(rename = "noteId")]
+    pub note_id: u64,
+    #[serde(rename = "noteAuthorParticipantId")]
+    pub participant_id: String,
+    #[serde(rename = "createdAtMillis")]
+    pub created_at_ms: u64,
+    #[serde(rename = "currentStatus")]
+    pub current_status: Status,
+}
+
+impl NoteStatusHistoryEntry {
+    pub fn read<P: AsRef<Path>>(path: P) -> Result<HashMap<u64, Self>, super::Error> {
+        let mut reader = csv::ReaderBuilder::new()
+            .has_headers(true)
+            .delimiter(b'\t')
+            .from_reader(File::open(path)?);
+        let mut entries = HashMap::new();
+
+        for entry in reader.deserialize::<Self>() {
+            let note_status_history_entry = entry?;
+
+            match entries.entry(note_status_history_entry.note_id) {
+                Entry::Occupied(_) => {
+                    return Err(super::Error::DuplicateNoteStatusHistoryEntry(
+                        note_status_history_entry.note_id,
+                    ));
+                }
+                Entry::Vacant(entry) => {
+                    entry.insert(note_status_history_entry);
+                }
+            }
+        }
+
+        Ok(entries)
+    }
+}
+
+#[derive(Clone, Debug, Eq, Ord, PartialEq, PartialOrd, serde::Deserialize, serde::Serialize)]
+pub struct NoteEntry {
+    #[serde(rename = "Note ID")]
+    pub note_id: u64,
+    #[serde(rename = "Created at", alias = "Timestamp")]
+    pub created_at_ms: u64,
+    #[serde(rename = "Alias")]
+    pub alias: Option<String>,
+    #[serde(rename = "Tweet ID")]
+    pub tweet_id: Option<u64>,
+    #[serde(rename = "User ID")]
+    pub user_id: Option<u64>,
+    #[serde(rename = "Misleading")]
+    pub misleading: Option<bool>,
+    #[serde(rename = "Helpful", alias = "Accepted")]
+    pub helpful: Option<bool>,
+}
+
+impl NoteEntry {
+    pub fn read<P: AsRef<Path>>(path: P) -> Result<HashMap<u64, Self>, super::Error> {
+        let mut paths = std::fs::read_dir(&path)?
+            .map(|entry| Ok(entry.map_err(super::Error::from)?.path()))
+            .collect::<Result<Vec<_>, super::Error>>()?;
+
+        paths.sort();
+
+        let mut entries = HashMap::new();
+
+        for path in paths {
+            let mut reader = csv::ReaderBuilder::new()
+                .has_headers(true)
+                .from_reader(File::open(path)?);
+
+            for entry in reader.deserialize::<Self>() {
+                let note_entry = entry?;
+
+                match entries.entry(note_entry.note_id) {
+                    Entry::Occupied(_) => {
+                        return Err(super::Error::DuplicateNoteEntry(note_entry.note_id));
+                    }
+                    Entry::Vacant(entry) => {
+                        entry.insert(note_entry);
+                    }
+                }
+            }
+        }
+
+        Ok(entries)
+    }
+
+    pub fn write<P: AsRef<Path>>(path: P, values: HashMap<u64, Self>) -> Result<(), super::Error> {
+        let mut values = values
+            .values()
+            .map(|note_entry| {
+                let timestamp =
+                    chrono::DateTime::from_timestamp_millis(note_entry.created_at_ms as i64)
+                        .ok_or_else(|| super::Error::InvalidTimestamp(note_entry.created_at_ms))?;
+
+                let month = timestamp.format("%Y-%m").to_string();
+
+                Ok((month, note_entry))
+            })
+            .collect::<Result<Vec<_>, super::Error>>()?;
+
+        values.sort();
+
+        for (month, month_values) in &values.into_iter().chunk_by(|(month, _)| month.clone()) {
+            let mut month_values = month_values
+                .map(|(_, note_entries)| note_entries)
+                .collect::<Vec<_>>();
+
+            month_values.sort();
+
+            let month_path = path.as_ref().join(format!("{}.csv", month));
+
+            let mut writer = csv::WriterBuilder::new()
+                .has_headers(true)
+                .from_writer(File::create(month_path)?);
+
+            for month_value in month_values {
+                writer.serialize(month_value)?;
+            }
+
+            writer.flush()?;
+        }
+
+        Ok(())
     }
 }
